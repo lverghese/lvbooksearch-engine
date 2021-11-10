ef8a6b02715599924c0c69589ea15f78abad2d85
lverghese <loganverghese@gmail.com>
2021-11-10 16:30:58 -0500

    login testing fix

--- server/schemas/TypeDefs.js
+++ server/schemas/TypeDefs.js
@@ -36,7 +36,7 @@ const typeDefs = gql`
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(input: inputBook): User
+        saveBook(input: bookInput): User
         removeBook(bookId: String!): User
     }
 `;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
 
 const { ApolloServer } = require('apollo-server-express')
 const { typeDefs, resolvers } = require('./schemas');
@@ -17,7 +16,7 @@ const server = new ApolloServer({
 });
 
 server.applyMiddleware({ app })
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
@@ -25,7 +24,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
